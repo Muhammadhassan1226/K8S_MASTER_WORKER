b0be349eb8bac3a90f190356153133412e48852f
Muhammadhassan1226 <127580211+Muhammadhassan1226@users.noreply.github.com>
2023-05-01 09:39:47 +0500

    Create worker.sh

---
+++ worker.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+apt update
+apt install -y apt-transport-https ca-certificates curl 
+curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt update
+apt install -y apt-transport-https ca-certificates curl 
+apt install -y kubelet kubeadm kubelet docker.io  
+
